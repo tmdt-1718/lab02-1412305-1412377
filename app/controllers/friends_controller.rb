6f08c0ab361ccc14b51f0183526be51ec439db61
nhatnt <nhatnt@vng.com.vn>
2017-10-24 22:57:47 +0700

    [TMDT-Lab02] - add delete friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,33 @@
 class FriendsController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def index
+
         @friends = Friend.all
         @users = User.all
+
         def is_friend(userid1, userid2)
             Friend.exists?(user1: userid1, user2: userid2)
         end
     end
+
+    def create
+        puts params[:user2]
+        @friend = Friend.new(user1: current_user.id,  user2: params[:user2])
+        
+        if @friend.save
+            redirect_to friends_path
+        else 
+            render :new
+        end
+
+    end
+
+    def destroy
+        puts params[:user2]
+        Friend.where(user1: current_user.id,  user2: params[:user2]).destroy_all
+        redirect_to friends_path
+    end
+    
     helper_method :is_friend
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   get '/login', to: 'sessions#new', as: 'login'
   get '/logout', to: 'sessions#destroy'
+  get '/friends/delete', to: 'friends#destroy'
   resources :friends 
   resources :users
   resources :sessions
